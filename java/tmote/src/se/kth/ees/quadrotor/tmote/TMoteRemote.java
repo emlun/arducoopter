6e4fca945e5b63507c75cffde0c94a917830ded8
Emil Lundberg <emlun@kth.se>
2012-07-10 15:17:22 +0200

    Improved interface usability

--- java/tmote/src/se/kth/ees/quadrotor/tmote/TMoteRemote.java
+++ java/tmote/src/se/kth/ees/quadrotor/tmote/TMoteRemote.java
@@ -1,5 +1,7 @@
 package se.kth.ees.quadrotor.tmote;
 
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 import java.io.IOException;
 import java.net.UnknownHostException;
 
@@ -8,17 +10,20 @@ import javax.swing.JFrame;
 import javax.swing.JSlider;
 import javax.swing.border.TitledBorder;
 
-public class TMoteRemote extends JFrame implements Runnable {
+public class TMoteRemote extends JFrame implements Runnable, MouseListener {
 
     public static final short DESTINATION_MOTE_ID = 13;
 
     private TMoteConnection con;
     private JSlider[] channelSliders;
-    
+    private boolean[] centering = new boolean[]{true, true, false, true, false,
+            false, false, false};
+
     byte[] payload = new byte[8];
 
     public TMoteRemote() {
 
+        setTitle("Virtual 8ch Radio Controller");
         setDefaultCloseOperation(EXIT_ON_CLOSE);
 
         setLayout(new BoxLayout(getContentPane(), BoxLayout.X_AXIS));
@@ -26,14 +31,17 @@ public class TMoteRemote extends JFrame implements Runnable {
         channelSliders = new JSlider[8];
         for(int i = 0; i < 8; i++) {
             channelSliders[i] = new JSlider(JSlider.VERTICAL, 0, 255, 0);
+            channelSliders[i].addMouseListener(this);
             add(channelSliders[i]);
-            
-            channelSliders[i].setBorder(new TitledBorder("" + (i+1)));
+
+            channelSliders[i].setBorder(new TitledBorder("" + (i + 1)));
             channelSliders[i].setMajorTickSpacing(50);
             channelSliders[i].setMinorTickSpacing(5);
             channelSliders[i].setPaintTicks(true);
             channelSliders[i].setPaintLabels(true);
         }
+
+        mouseReleased(null);
 
         try {
             con = new TMoteConnection();
@@ -72,6 +80,20 @@ public class TMoteRemote extends JFrame implements Runnable {
                 Thread.sleep(98);
             } catch(InterruptedException e) {
                 e.printStackTrace();
+            }
+        }
+    }
+
+    @Override public void mouseClicked(MouseEvent arg0) {}
+    @Override public void mouseEntered(MouseEvent arg0) {}
+    @Override public void mouseExited(MouseEvent arg0) {}
+    @Override public void mousePressed(MouseEvent arg0) {}
+
+    @Override
+    public void mouseReleased(MouseEvent event) {
+        for(int i = 0; i < 8; i++) {
+            if(centering[i]) {
+                channelSliders[i].setValue(127);
             }
         }
     }
